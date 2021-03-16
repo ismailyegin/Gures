4b2fbe7e3f7cf23fbd476db37a02d88845f62444
fatih-berktug <faatihberktug@hotmail.com>
2021-03-16 15:20:17 +0300

    müsabaka

--- sbs/models/CompetitionsDocument.py
+++ sbs/models/CompetitionsDocument.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class CompetitionsDocument(models.Model):
+    file = models.FileField(upload_to='document/', null=False, blank=False, verbose_name='Document')
     creationDate = models.DateTimeField(auto_now_add=True)
     operationDate = models.DateTimeField(auto_now=True)
-    name = models.FileField(upload_to='competitions/', null=False, blank=False, verbose_name='file')
 
-    def __str__(self):
-        return '%s' % (self.name)
+    # def __str__(self):
+    #     return '%s' % (self.name)
