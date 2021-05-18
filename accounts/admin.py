e7ea2e538a0b52f9a18805a574c78278c9afee1a
Fatih Berktug <fatihberktug>
2021-05-18 17:30:55 +0300

    admin modülüne model güncellemesi eklendi

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,6 +4,7 @@ from sbs.models.Competitiontype import Competitiontype
 from sbs.models.Category import Category
 from sbs.models.CompetitionStil import CompetitionStil
 from sbs.models.ActivityType import ActivityType
+from sbs.models.Competition import Competition
 
 admin.site.site_header = 'Kobiltek Bilisim Kullanici Yönetim Paneli '  # default: "Django Administration"
 admin.site.index_title = 'Sistem Yönetimi'  # default: "Site administration"
@@ -13,3 +14,4 @@ admin.site.register(Competitiontype)
 admin.site.register(Category)
 admin.site.register(CompetitionStil)
 admin.site.register(ActivityType)
+admin.site.register(Competition)
